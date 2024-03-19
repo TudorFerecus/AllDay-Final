2f2291f713a173c1ab603bf03322a675762f5769
Tudor Ferecus <tudorferecus@gmail.com>
2024-03-19 21:42:57 +0200

    preparing for deploy

--- client/src/features/Context/ContextValues.js
+++ client/src/features/Context/ContextValues.js
@@ -4,7 +4,7 @@ import { AppContext } from "./Context"
 function ContextProvider(props)
 {
     const [datesSelected, setDatesSelected] = useState([])
-    const LOCAL_LINK = 'http://10.41.238.53:3000'
+    const LOCAL_LINK = 'http://192.168.0.88:3000'
     const REMOTE_LINK = 'https://main--all-day.netlify.app'
     const values = {datesSelected, setDatesSelected, LOCAL_LINK, REMOTE_LINK}
 
